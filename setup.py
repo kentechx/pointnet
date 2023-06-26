f9fcbe092d53dc2e49a781446e3128dcc4b7f7f8
Kentechx <kd_shen@163.com>
2023-06-27 03:34:42 +0800

    version 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pointnet',
     packages=find_packages(),
-    version='0.0.4',
+    version='0.1.1',
     license='MIT',
     description='PointNet - Pytorch',
     author='Kaidi Shen',
